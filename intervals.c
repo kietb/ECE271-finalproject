318d055f5d47552de437898d9ec60cc74168c7f8
Kiet Bui <buiki@oregonstate.edu>
2017-11-27 17:48:30 -0800

    finish sin save module and fix intervals # to 525

--- intervals.c
+++ intervals.c
@@ -3,15 +3,10 @@
 #include <math.h>
 
 int main( ) {
-  for (int i =0; i<500; i++) {
-    printf("real t%d;\n",i);
-  
-}
-  for(int i=0; i<500;i++) {
-  double x = 127.5*sin(2*3.1415926/500*i)+127;
-  printf("assign t%d <= %3.0f;\n",i,x);
+
+for(int i=0; i<525;i++) {
+  double x = 127.5*sin(2*3.1415926/525*i)+127;
+  printf("%d: wave <=%3.0f;\n",i,x);
   }
   return 0;
 }
-
-
